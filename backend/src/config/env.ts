c3f7da92c6d95bb26a8e2916ef5bd2fd6225cb19
vishnupriyan207 <vishnupriyan207@gmail.com>
2026-06-12 13:08:43 +0530

    routes

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -6,7 +6,7 @@ export const ENV = {
   PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  FRONTEND_URL: process.env.FRONEND_URL,
+  FRONTEND_URL: process.env.FRONTEND_URL,
   CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
 };
